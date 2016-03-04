9b374670c1b845669013e7977eca98f7b9080d0f
ICEon <matrix_eon@yahoo.com>
2016-03-04 01:39:28 -0600

    nueve

--- js/acciones.js
+++ js/acciones.js
@@ -67,7 +67,7 @@ correo = $('#correoe').val();
 procedencia = $('#procedenciaa').val();
 carrera = $('#scarrera').val();
 
-
+alert (nombre);
 db.transaction(function(tx) {
               tx.executeSql("INSERT INTO registros (nombre, correo, procedencia, carrera) VALUES (?,?,?,?)", [nombre, correo, procedencia, carrera], function(tx, res) {
 				  				  $("#confirmacion").popup();
